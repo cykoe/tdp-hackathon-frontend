6224790c66dc8f9a7825fad89ff7cc081fcc9969
FIRST_NAME LAST_NAME <esculin.hao@gmail.com>
2019-07-25 15:25:12 -0500

    new graphs

--- src/components/radarChart.js
+++ src/components/radarChart.js
@@ -1,70 +1,36 @@
 import React from 'react';
-// import * as d3 from 'd3';
 import Radar from 'react-d3-radar';
 
 class RadarChart extends React.Component {
 
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
     this.state = {
       username: ''
     };
-    this.fetchCountry = this.fetchCountry.bind(this)
   }
-
-  fetchCountry () {
-    // const country = 'ghana';
-    // const options = {
-    //   method: 'GET',
-    //   credentials: "same-origin", //include, same-origin
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    // };
-    // fetch(`${cor}${url}?country=${country}`, options)
-    //   .then(response => {
-    //     console.log(response);
-    //     this.setState({username: response});
-    //   })
-  }
-
-  // componentDidMount () {
-  //   this.drawChart();
-  // }
-  //
-  // drawChart(){
-  //   const svg = d3
-  //     .create('svg')
-  //     .attr('viewBox', [0,0,this.props.width, this.props.height]);
-  //
-  //   svg
-  //     .append('g')
-  //     .selectAll('g')
-  //     .data()
-  //
-  // }
 
   render () {
     let data = {
       variables: this.props.variables,
       sets: this.props.sets,
     };
-    console.log(data);
     return (
       <div>
+        <button type="button" className="btn btn-primary">{data.sets[0].label}</button>
+        <button type="button" className="btn btn-warning">{data.sets[1].label}</button>
         <Radar
-          width={700}
-          height={350}
-          padding={70}
-          domainMax={400}
+          width={500}
+          height={400}
+          padding={80}
+          domainMax={3}
           highlighted={null}
           onHover={(point) => {
-            if (point) {
-              console.log('hovered over a data point');
-            } else {
-              console.log('not over anything');
-            }
+            // if (point) {
+            //   console.log('hovered over a data point');
+            // } else {
+            //   console.log('not over anything');
+            // }
           }}
           data={data}
         />
