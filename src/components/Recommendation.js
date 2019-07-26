290b81572163a73c452a41889c208ca4413fe44a
Yang <yifan.b.yang@accenture.com>
2019-07-26 00:53:23 -0500

    added rec bg and some text

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -10,13 +10,13 @@ class Recommendation extends React.Component {
 
   render () {
     return (<Page>
-      <div className='Component-Bg'><h1>Customized Recommendation</h1>
-        <div id="app">
+      <div className='bg_pic'><h1>Your Customized Recommendation to pack for Ghana</h1>
+        <div className="app">
           <div style={{marginLeft: '5%'}}>
             <div style={{textAlign: 'center'}}><Card> <CardImg top width="100%"
                                                                src="https://media.istockphoto.com/photos/pile-of-raw-basmati-rice-with-a-spoon-picture-id519309790?k=6&m=519309790&s=612x612&w=0&h=wMK6lbBadGe4dC13B5htyB1IDq1AHfaNwZR-vnKWbD8="
                                                                alt="Card image cap"/> <CardBody>
-              <div style={{padding: '5 %'}}><CardTitle>Ghana's main focus areas are...</CardTitle>
+              <div style={{padding: '5 %'}}><CardTitle>Ghana's main nutrient deficients are...</CardTitle>
                 <div style={{textAlign: 'left'}}><CardText>
                   <ul style={{marginLeft: '2%'}}>
                     <li>Fats</li>
